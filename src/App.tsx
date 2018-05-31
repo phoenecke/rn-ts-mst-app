cfcf5db39b1130d7258dffd51cdf80de4b3597d4
Paul Hoenecke <paulh@spindance.com>
2018-05-31 15:25:28 -0400

    linting and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component<{}> {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.ts to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +20,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
